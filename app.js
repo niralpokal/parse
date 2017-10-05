13fda0b4b6b6d0fda83e2bb76798dc80b0b75cde
niralpokal <niralpokal@gmail.com>
2017-10-04 20:29:12 -0700

    Updated app.js to use extractText. Added extractText files to library.

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
-const PDFParser = require('pdf2json');
-let pdfParser = new PDFParser();
+const hummus = require('hummus');
+const _ = require('lodash');
+const extractText = require('./lib/text-extraction');
 
-   pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-   pdfParser.on("pdfParser_dataReady", pdfData => {
-       console.log(pdfData);
-       fs.writeFile("./output/myFile.json", JSON.stringify(pdfData));
-       console.log(pdfData);
-   });
+let parsePages = () => {
+    let fileToRun = './input/myFile.pdf';
+    let pdfReader = hummus.createReader(fileToRun);
+    let pagesPlacements = extractText(pdfReader);
+    return pagesPlacements;
+}
 
-   pdfParser.loadPDF("./input/myFile.pdf");
+let pages = parsePages();
+pages[0].forEach((item)=>{
+   console.log(item.text);
+});
+console.log(pages.length)
+
 
