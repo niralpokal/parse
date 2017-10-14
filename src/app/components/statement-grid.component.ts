3da8601662a6f06906b2721b7156fa2a1b7a0b57
niralpokal <niralpokal@gmail.com>
2017-10-14 15:35:08 -0700

    Added statement grid component.

---
+++ src/app/components/statement-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'statement-grid',
+    template:`
+    <div> Template Grid </div>
+    `,
+    styleUrls:[]
+})
+
+export class StatementGridComponent {}
