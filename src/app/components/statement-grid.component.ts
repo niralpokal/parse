eb7aaf445fafffc7d9738617268910026330a896
niralpokal <niralpokal@gmail.com>
2017-10-16 19:46:21 -0700

    Added mock data for template grid.

--- src/app/components/statement-grid.component.ts
+++ src/app/components/statement-grid.component.ts
@@ -3,9 +3,35 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'statement-grid',
     template:`
-    <div> Template Grid </div>
+    <div class="grid"> 
+        <div class="grid-row">
+            <p *ngFor="let header of headings" class="grid-cell"> {{header}} </p>
+        </div>
+        <div *ngFor="let item of gridItems" class="grid-row">
+            <div class="grid-cell">{{item.date}}</div>
+            <div class="grid-cell">{{item.location}}</div>
+            <div class="grid-cell">{{item.amount}}</div>
+        </div>
+    </div>
     `,
-    styleUrls:[]
+    styleUrls:['statement-grid.component.scss']
 })
 
-export class StatementGridComponent {}
+export class StatementGridComponent {
+    headings = ['Date', 'Location', 'Amount']
+    gridItems = [{
+        date: new Date(),
+        location: 'NY',
+        amount: '107.23'
+    },
+    {
+        date: new Date(),
+        location: 'NY',
+        amount: '107.23'
+    },
+    {
+        date: new Date(),
+        location: 'NY',
+        amount: '107.23'
+    }]
+}
